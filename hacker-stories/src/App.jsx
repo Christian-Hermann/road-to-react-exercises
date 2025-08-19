7f245f5f5cea2f6cb9328ca39591f1facb1ad8e2
Your NameChristian <christianhermanndev@gmail.com>
2025-08-19 17:20:51 -0500

    Meet another React Component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -22,25 +22,38 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <List />
     </div>
   );
 }
 
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
 export default App;
